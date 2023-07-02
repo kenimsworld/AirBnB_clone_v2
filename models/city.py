8b387c0f03807365ffdef87c7902d95a341a403c
abel-udoh <abeludoh8@gmail.com>
2023-07-01 23:44:25 -0700

    airbnb clone v2

--- models/city.py
+++ models/city.py
@@ -1,22 +1,18 @@
 #!/usr/bin/python3
 """ City Module instances cities with ids """
-from sqlalchemy import Column, Integer, String, ForeignKey
-from models.base_model import BaseModel, Base
-from os import getenv
+from sqlalchemy import String, DateTime, Column, ForeignKey
 from sqlalchemy.orm import relationship
-
-STORAGE = getenv("HBNB_TYPE_STORAGE")
+from models.base_model import BaseModel, Base
 
 
 class City(BaseModel, Base):
-    """ The city class, contains state ID and name """
+    """This is the class for City
+    Attributes:
+        state_id: The state id
+        name: input name
+    """
     __tablename__ = 'cities'
-    if STORAGE == "db":
-        state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
-        name = Column(String(128), nullable=False)
-        places = relationship('Place',
-                              backref='cities',
-                              cascade="all, delete-orphan")
-    else:
-        name = ""
-        state_id = ""
+    name = Column(String(128), nullable=False)
+    state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
+    places = relationship('Place', backref='cities',
+                          cascade='all, delete-orphan')
