7000fce11b9f3336dc6d9fa2df360945e676525c
abel-udoh <abeludoh8@gmail.com>
2023-07-12 23:08:44 -0700

    airbnb deploy[2~

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -3,34 +3,17 @@
 This is a Fabric script that generates a tgz archive from the
 contents of the web_static folder of the AirBnB Clone repo
 """
-
-
 import os
+from fabric.operations import local
 from datetime import datetime
-from fabric.api import local, runs_once
 
 
-@runs_once
 def do_pack():
-
-    """Archives the static files."""
-
-    if not os.path.isdir("versions"):
-        os.mkdir("versions")
-    d_time = datetime.now()
-    output = "versions/web_static_{}{}{}{}{}{}.tgz".format(
-        d_time.year,
-        d_time.month,
-        d_time.day,
-        d_time.hour,
-        d_time.minute,
-        d_time.second
-    )
-    try:
-        print("Packing web_static to {}".format(output))
-        local("tar -cvzf {} web_static".format(output))
-        size = os.stat(output).st_size
-        print("web_static packed: {} -> {} Bytes".format(output, size))
-    except Exception:
-        output = None
-    return output
+    """Function to compress files"""
+    local("mkdir -p versions")
+    result = local("tar -cvzf versions/web_static_{}.tgz web_static"
+                   .format(datetime.strftime(datetime.now(), "%Y%m%d%H%M%S")),
+                   capture=True)
+    if result.failed:
+        return None
+    return result
